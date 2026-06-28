4f993164dddd62b59c3e64061ed22dc109ad7c1b
choktisit2532-code <choktisit2532@gmail.com>
2026-06-28 12:26:43 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get("/customers", async (req, res) => {
 });
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
