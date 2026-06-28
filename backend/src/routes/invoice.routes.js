44a0f89b9bff9efe089563c1e536933f135b37b1
choktisit2532-code <choktisit2532@gmail.com>
2026-06-28 20:52:36 +0700

    Delete backend directory

--- backend/src/routes/invoice.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const invoiceController = require('../controllers/invoice.controller');
-const { verifyToken, authorizeRoles } = require('../middleware/auth.middleware');
-
-router.post('/', verifyToken, authorizeRoles('admin', 'staff'), invoiceController.createInvoice);
-router.get('/:id/pdf', verifyToken, invoiceController.generatePDF);
-
-module.exports = router;
