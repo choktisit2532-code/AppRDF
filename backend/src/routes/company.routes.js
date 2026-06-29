bd92968aae6fee505ba8a1d84cd4242226eab412
choktisit2532-code <choktisit2532@gmail.com>
2026-06-30 01:36:27 +0700

    Delete backend directory

--- backend/src/routes/company.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getCompanySettings, updateCompanySettings } from '../controllers/company.controller.js';
-import { authenticate, authorize } from '../middleware/auth.middleware.js';
-import { asyncHandler } from '../utils/async-handler.js';
-
-export const companyRouter = Router();
-companyRouter.use(authenticate);
-companyRouter.get('/', asyncHandler(getCompanySettings));
-companyRouter.patch('/', authorize('admin'), asyncHandler(updateCompanySettings));
--- backend/src/utils/app-error.js
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  constructor(statusCode, message, code = 'APPLICATION_ERROR', details) {
-    super(message);
-    this.name = 'AppError';
-    this.statusCode = statusCode;
-    this.code = code;
-    this.details = details;
-  }
-}
